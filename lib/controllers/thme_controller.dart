9a1e33b101dbc2e421392936c37529552d4dec0f
unknown <241730042.fadlihifziansyah@uinbanten.ac.id>
2026-04-21 09:06:56 +0700

    add: icon

--- lib/controllers/thme_controller.dart
+++ lib/controllers/thme_controller.dart
@@ -4,6 +4,16 @@ import 'package:flutter/material.dart';
 class ThemeController extends GetxController {
   var isDark = false.obs;
 
+  @override
+  void onInit() {
+    super.onInit();
+
+    final brightness =
+        WidgetsBinding.instance.platformDispatcher.platformBrightness;
+
+    isDark.value = brightness == Brightness.dark;
+  }
+
   void toggleTheme() {
     isDark.value = !isDark.value;
     Get.changeThemeMode(isDark.value ? ThemeMode.dark : ThemeMode.light);
